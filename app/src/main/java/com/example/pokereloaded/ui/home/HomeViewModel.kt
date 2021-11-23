61f03442b92dbdd33b02b75772e89b6cbcd52fdb
Juan Cisilino <54241344+JuanCisilino@users.noreply.github.com>
2021-11-23 15:18:34 -0300

    Implementacion pedido favoritos y paginacion

--- app/src/main/java/com/example/pokereloaded/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/pokereloaded/ui/home/HomeViewModel.kt
@@ -2,9 +2,15 @@ package com.example.pokereloaded.ui.home
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.example.pokereloaded.models.Pokemon
 import com.example.pokereloaded.network.PokeRepo
 import com.example.pokereloaded.network.RetroInstance
+import kotlinx.coroutines.flow.Flow
 import rx.schedulers.Schedulers
 
 class HomeViewModel : ViewModel() {
@@ -23,6 +29,11 @@ class HomeViewModel : ViewModel() {
                     pokemonList.postValue(it.pokemonList)
                 },
                 {pokemonList.postValue(null)})
+    }
+
+    fun fetchPaginatedList(): Flow<PagingData<Pokemon>> {
+        return Pager(config = PagingConfig(pageSize = 50, maxSize = 100, prefetchDistance = 20),
+        pagingSourceFactory = {PokemonPagingSource(instance)}).flow.cachedIn(viewModelScope)
     }
 
 }
---
+++ app/src/main/java/com/example/pokereloaded/ui/home/PokemonPagingSource.kt
@@ -0,0 +1,40 @@
+package com.example.pokereloaded.ui.home
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.pokereloaded.models.Pokemon
+import com.example.pokereloaded.network.PokeRepo
+import java.lang.Exception
+
+class PokemonPagingSource(val repo: PokeRepo): PagingSource<Int, Pokemon>() {
+
+    companion object{
+        private const val FIRST_PAGE_INDEX = 1
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Pokemon>) : Int?{
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Pokemon> {
+        return try {
+            var nextPage = params.key ?: FIRST_PAGE_INDEX
+            val response = repo.getList(nextPage)
+            nextPage++
+            return when (nextPage) {
+                25 -> LoadResult.Error(Throwable("no more pages"))
+                else -> {
+                    LoadResult.Page(
+                        data = response.pokemonList,
+                        prevKey = null,
+                        nextKey = nextPage)
+                }
+            }
+        } catch (e: Exception){
+            LoadResult.Error(e)
+        }
+    }
+}
